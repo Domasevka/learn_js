200d73281645334581846380d839cf8ba1bf19ec
Marina Domasevich <m.domasevich@andersenlab.com>
2017-06-20 06:19:03 +0200

    change_2 to task_3

--- js/init.js
+++ js/init.js
@@ -2,27 +2,24 @@
 
 $(document).ready(function() {
 
-var countries = ['United States', 'Canada', 'Argentina', 'Armenia'];
-var list =$('<ul/>');
-$.each(countries, function(i)
-{
-    var li = $('<li/>')
-        .addClass('ui-menu-item')
-        .attr('role', 'menuitem')
-        .appendTo(list);
-    var aaa = $('<a/>')
-        .addClass('ui-all')
-        .text(countries[i])
-        .appendTo(li);
-
-});
-
-
     $( "#target" ).on('click', function(e) {
         e.preventDefault();
+
+        var countries = ['United States', 'Canada', 'Argentina', 'Armenia'];
+        var list =$('<ul/>');
+        $.each(countries, function(i)
+        {
+            var item = $('<li/>')
+                .addClass('ui-menu-item')
+                .attr('role', 'menuitem')
+                .appendTo(list);
+            var link = $('<a/>')
+                .addClass('ui-all')
+                .text(countries[i])
+                .appendTo(item);
+        });
         $("div.mylist").append(list);
     });
-
 });
 
 
