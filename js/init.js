15d8ae67814def1b203b649d78337c83125f0250
Marina Domasevich <m.domasevich@andersenlab.com>
2017-06-26 22:49:08 +0200

    adding ul class

--- js/init.js
+++ js/init.js
@@ -5,23 +5,28 @@ $(document).ready(function() {
     $( "#target" ).on('click', function(e) {
         e.preventDefault();
 
-    if ($('ul').length) {
+    if ($('ul.list').length) {
         alert("Привет");
+
     } else {
-        var countries = ['United States', 'Canada', 'Argentina', 'Armenia'];
-        var list =$('<ul/>');
-        $.each(countries, function(i)
-        {
-            var item = $('<li/>')
-                .addClass('ui-menu-item')
-                .attr('role', 'menuitem')
-                .appendTo(list);
-            var link = $('<a/>')
-                .addClass('ui-all')
-                .text(countries[i])
-                .appendTo(item);
+        $(function(){
+            var countries = ['United States', 'Canada', 'Argentina', 'Armenia'];
+            var list =$('<ul/>')
+                .addClass('list');
+            $.each(countries, function(i)
+            {
+                var item = $('<li/>')
+                    .addClass('list__item')
+                    .attr('role', 'menuitem')
+                    .appendTo(list);
+                var link = $('<a/>')
+                    .addClass('list__link')
+                    .text(countries[i])
+                    .appendTo(item);
+            });
+            $("div.mylist").append(list);
         });
-        $("div.mylist").append(list);
+
     }
     });
 });
